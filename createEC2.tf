669034e074936175ba838b5d57710b2bce5ac7d5
Colin Baird <colinbaird89@gmail.com>
2022-04-26 13:45:40 -0500

    Update createEC2.tf

--- createEC2.tf
+++ createEC2.tf
@@ -1,15 +1,15 @@
-data "aws_vpc" "primary" {
-
-  filter {
-    name = "tag:${local.env_profile}-vpc"
-    values = ["${local.env_profile}-vpc"]
-  }
-
 resource "aws_instance" "instance" {
   ami                         = "ami-0c02fb55956c7d316"
   availability_zone           = "${var.aws_region}a"
   instance_type               = "t2.micro"
   associate_public_ip_address = true
-  vpc = "${data.aws_vpc.primary.id}"
+  
+  tags = {
+    name = "test-EC2"
+    owner = "cbaird"
+    expire-on = "2022-05-10"
+    purpose = "training"
+    
+  }
 }
 
