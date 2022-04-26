15419d972cb6504f33a0d48af316b70c4301d64e
Colin Baird <colinbaird89@gmail.com>
2022-04-26 11:37:18 -0500

    Update createEC2.tf
    Add logic to deploy EC2 instance in newly created VPC

--- createEC2.tf
+++ createEC2.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "instance" {
   availability_zone           = "${var.aws_region}a"
   instance_type               = "t2.micro"
   associate_public_ip_address = true
-  
+  vpc = "${data.aws_vpc.primary.id}"
 }
 
