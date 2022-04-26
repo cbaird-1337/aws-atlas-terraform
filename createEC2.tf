7e42879e6dc4678f4316300ff5e6dc6cffc2ccdd
Colin Baird <colinbaird89@gmail.com>
2022-04-26 13:53:35 -0500

    Update createEC2.tf
    Commenting out this segment

--- createEC2.tf
+++ createEC2.tf
@@ -1,15 +1,18 @@
-resource "aws_instance" "instance" {
-  ami                         = "ami-0c02fb55956c7d316"
-  availability_zone           = "${var.aws_region}a"
-  instance_type               = "t2.micro"
-  associate_public_ip_address = true
+#This creates an ec2 instance with tags but does NOT contain the logic necessary to place the instance in the newly created VPC, or pull the pem key file.
+#Uncomment the sections below if you want to create the ec2 instance along with the rest of this script
+
+#resource "aws_instance" "instance" {
+  #ami                         = "ami-0c02fb55956c7d316"
+  #availability_zone           = "${var.aws_region}a"
+  #instance_type               = "t2.micro"
+  #associate_public_ip_address = true
   
-  tags = {
-    name = "test-EC2"
-    owner = "name_here"
-    expire-on = "expiry_date"
-    purpose = "training"
+  #tags = {
+    #name = "test-EC2"
+    #owner = "name_here"
+    #expire-on = "expiry_date"
+    #purpose = "training"
     
-  }
-}
+  #}
+#}
 
